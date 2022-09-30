902324e919d79655096f000ba320868db461fcf2
Hayato Arai <arai-hayato673@g.ecc.u-tokyo.ac.jp>
2022-09-30 17:55:40 +0900

    単項演算子+-を実装した

--- aralc.c
+++ aralc.c
@@ -141,7 +141,8 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
-Node *primary();
+Node *unary();    //\pm primary
+Node *primary();  // num or (expr)
 
 Node *expr() {
     //今見てるトークンからexprの構文木を作成する
@@ -158,15 +159,24 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
     while (1) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    }
+    if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
 }
 
 Node *primary() {
