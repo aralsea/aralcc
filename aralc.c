16e0a3148c670b5205a473adac69d78b3a30a6d3
Hayato Arai <arai-hayato673@g.ecc.u-tokyo.ac.jp>
2022-09-30 17:06:28 +0900

    再帰下降構文解析により四則演算をコンパイルできるようになった

--- aralc.c
+++ aralc.c
@@ -90,13 +90,15 @@ Token *tokenize() {
             p++;
             continue;
         }
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+            *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             //,cur, p);
             // p++; と書いても同じ
             continue;
         }
         // Q. new_token()にpを代入するとEOFが遠くの方にあるのでは？
+        // A.はい．pは「そのトークンの始まる位置」なので
         if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
             cur->val = strtol(p, &p, 10);
@@ -110,6 +112,107 @@ Token *tokenize() {
 }
 //トークナイザここまで
 
+//パーサ
+typedef enum { ND_ADD, ND_SUB, ND_MUL, ND_DIV, ND_NUM } NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;  //左の子
+    Node *rhs;  //右の子
+    int val;    // kind=ND_NUMのとき，その値
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    //今見てるトークンからexprの構文木を作成する
+    //返り値はその木の根を表すNode
+    Node *node = mul();
+    while (1) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+    while (1) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+//パーサ終わり
+
+//アセンブリジェネレータ
+void gen(Node *node) {
+    // node を根とする構文木からアセンブリを生成する
+    // 計算結果はスタックトップに保存される
+
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません．\n");
@@ -118,22 +221,15 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    printf("    mov rax, %d\n", expect_number());
+    gen(node);
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
